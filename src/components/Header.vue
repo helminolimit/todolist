fd4ba13334b51648c4c07c2023168153b92d8171
helminolimit <58017701+helminolimit@users.noreply.github.com>
2023-02-22 08:57:09 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Listing" color="green" />
+        <!--<Button text="Update Listing" color="blue" />
+        <Button text="Delete Listing" color="red" />-->
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'THis is default'
+        }
+    },
+    components: {
+        Button
+    }
+}
+
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
